e8b5da94fcff6ac844b84f04e073da3896907fe7
Forrest Laine <forrest.laine@vanderbilt.edu>
2023-02-22 18:05:15 -0600

    cleaning up

--- src/view_car.jl
+++ src/view_car.jl
@@ -83,39 +83,13 @@ function spawn_car(vis, sock)
 end
         
 
-function car_sim_server(vis, host::IPAddr = IPv4(0), port=3000)
+function car_sim_server(vis, host::IPAddr = IPv4(0), port=4444)
     errormonitor(@async begin
         server = listen(host, port)
         while true
             sock = accept(server)
             println("Client accepted!", sock)
             @async spawn_car(vis, sock)
-
-            #@async while isopen(sock)
-            #    write(sock, readline(sock, keep=true))
-            #end
         end
     end)
 end
-     
-
-
-#function sim_car(vis)
-#    urdf_path = joinpath(dirname(pathof(VehicleSim)), "assets", "chevy.urdf")
-#    chevy = parse_urdf(urdf_path)
-#    state = MechanismState(chevy)
-#    body_to_world = first(joints(chevy))
-#    mvis = MechanismVisualizer(chevy, URDFVisuals(urdf_path, package_path=[dirname(pathof(VehicleSim))]), vis)
-#    @infiltrate
-#    for t = 1:100
-#        θ = (t-1)*2*pi/100
-#        rot = RotXYZ(θ, 0, θ)
-#        quat = convert(UnitQuaternion{Float64}, rot)
-#        set_configuration!(state, body_to_world, [quat.w, quat.x, quat.y, quat.z, t, 0, 0])
-#        set_configuration!(mvis, configuration(state))
-#        sleep(0.01)
-#    end
-#end
-
-
-
