fdfa1ae6fe118bbb344c701ec56b221e89d30899
Forrest <forrest.laine@vanderbilt.edu>
2025-03-28 09:58:40 -0500

    bells and whistles for the example project

--- src/example_project.jl
+++ src/example_project.jl
@@ -8,9 +8,42 @@ struct MyPerceptionType
     field2::Float64
 end
 
-function localize(gps_channel, imu_channel, localization_state_channel)
+function process_gt(
+        gt_channel,
+        shutdown_channel,
+        localization_state_channel,
+        perception_state_channel)
+
+    while true
+        fetch(shutdown_channel) && break
+
+        fresh_gt_meas = []
+        while isready(gt_channel)
+            meas = take!(gt_channel)
+            push!(fresh_gt_meas, meas)
+        end
+
+        # process the fresh gt_measurements to produce localization_state and
+        # perception_state
+        
+        take!(localization_state_channel)
+        put!(localization_state_channel, new_localization_state_from_gt)
+        
+        take!(perception_state_channel)
+        put!(perception_state_channel, new_perception_state_from_gt)
+    end
+end
+
+function localize(
+        gps_channel, 
+        imu_channel, 
+        localization_state_channel, 
+        shutdown_channel)
     # Set up algorithm / initialize variables
     while true
+
+        fetch(shutdown_channel) && break
+
         fresh_gps_meas = []
         while isready(gps_channel)
             meas = take!(gps_channel)
@@ -23,18 +56,17 @@ function localize(gps_channel, imu_channel, localization_state_channel)
         end
         
         # process measurements
-
-        localization_state = MyLocalizationType(0,0.0)
-        if isready(localization_state_channel)
-            take!(localization_state_channel)
-        end
+        take!(localization_state_channel)
         put!(localization_state_channel, localization_state)
     end 
 end
 
-function perception(cam_meas_channel, localization_state_channel, perception_state_channel)
+function perception(cam_meas_channel, localization_state_channel, perception_state_channel, shutdown_channel)
     # set up stuff
     while true
+        
+        fetch(shutdown_channel) && break
+
         fresh_cam_meas = []
         while isready(cam_meas_channel)
             meas = take!(cam_meas_channel)
@@ -55,11 +87,15 @@ end
 
 function decision_making(localization_state_channel, 
         perception_state_channel, 
+        target_segment_channel,
+        shutdown_channel,
         map, 
-        target_road_segment_id, 
         socket)
     # do some setup
     while true
+
+        fetch(shutdown_channel) && break
+
         latest_localization_state = fetch(localization_state_channel)
         latest_perception_state = fetch(perception_state_channel)
 
@@ -76,7 +112,7 @@ function isfull(ch::Channel)
 end
 
 
-function my_client(host::IPAddr=IPv4(0), port=4444)
+function my_client(host::IPAddr=IPv4(0), port=4444; use_gt=false)
     socket = Sockets.connect(host, port)
     map_segments = VehicleSim.city_map()
     
@@ -88,11 +124,16 @@ function my_client(host::IPAddr=IPv4(0), port=4444)
     cam_channel = Channel{CameraMeasurement}(32)
     gt_channel = Channel{GroundTruthMeasurement}(32)
 
-    #localization_state_channel = Channel{MyLocalizationType}(1)
-    #perception_state_channel = Channel{MyPerceptionType}(1)
+    localization_state_channel = Channel{MyLocalizationType}(1)
+    perception_state_channel = Channel{MyPerceptionType}(1)
+    target_segment_channel = Channel{Int}(1)
+    shutdown_channel = Channel{Bool}(1)
+    put!(shutdown_channel, false)
 
     target_map_segment = 0 # (not a valid segment, will be overwritten by message)
     ego_vehicle_id = 0 # (not a valid id, will be overwritten by message. This is used for discerning ground-truth messages)
+
+    put!(target_segment_channel, target_map_segment)
 
     errormonitor(@async while true
         # This while loop reads to the end of the socket stream (makes sure you
@@ -111,6 +152,11 @@ function my_client(host::IPAddr=IPv4(0), port=4444)
         end
         !received && continue
         target_map_segment = measurement_msg.target_segment
+        old_target_segment = fetch(target_segment_channel)
+        if target_map_segment ≠ old_target_segment
+            take!(target_segment_channel)
+            put!(target_segment_channel, target_map_segment)
+        end
         ego_vehicle_id = measurement_msg.vehicle_id
         for meas in measurement_msg.measurements
             if meas isa GPSMeasurement
@@ -125,7 +171,51 @@ function my_client(host::IPAddr=IPv4(0), port=4444)
         end
     end)
 
-    @async localize(gps_channel, imu_channel, localization_state_channel)
-    @async perception(cam_channel, localization_state_channel, perception_state_channel)
-    @async decision_making(localization_state_channel, perception_state_channel, map, socket)
+    if use_gt
+        @async process_gt(gt_channel,
+                      shutdown_channel,
+                      localization_state_channel,
+                      perception_state_channel)
+    else
+        @async localize(gps_channel, 
+                    imu_channel, 
+                    localization_state_channel, 
+                    shutdown_channel)
+
+        @async perception(cam_channel, 
+                      localization_state_channel, 
+                      perception_state_channel, 
+                      shutdown_channel)
+    end
+
+
+
+    @async decision_making(localization_state_channel, 
+                           perception_state_channel, 
+                           target_segment_channel, 
+                           shutdown_channel,
+                           map, 
+                           socket)
+end
+
+function shutdown_listener(shutdown_channel)
+    info_string = 
+        "***************
+      CLIENT COMMANDS
+      ***************
+            -Make sure focus is on this terminal window. Then:
+            -Press 'q' to shutdown threads. 
+    "
+    @info info_string
+    while true
+        sleep(0.1)
+        key = get_c()
+
+        if key == 'q'
+            # terminate threads
+            take!(shutdown_channel)
+            put!(shutdown_channel, true)
+            break
+        end
+    end
 end
